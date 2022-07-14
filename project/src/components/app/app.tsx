efab4babe5a13900108775c8fa73662c72c9b652
socialcolor <64646814+socialcolor@users.noreply.github.com>
2022-07-14 15:26:37 +0300

    Создал компонент главной страницы и комопнент карточки

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -1,5 +1,11 @@
-function App(): JSX.Element {
-  return <p>Hello, world!</p>;
+import Main from '../../pages/main/main';
+
+type AppScreenProps = {
+  placesCount: number;
+}
+
+function App({placesCount}: AppScreenProps): JSX.Element {
+  return <Main placesCount = {placesCount} />;
 }
 
 export default App;
