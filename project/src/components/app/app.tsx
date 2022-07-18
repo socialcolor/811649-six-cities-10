2e06f07ebbed47e6954a9dcdbee663cbd7f87a69
socialcolor <64646814+socialcolor@users.noreply.github.com>
2022-07-18 21:33:22 +0300

    Добавил компонент svg

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -1,9 +1,16 @@
+import React from 'react';
+import IconArrowSelected from '../icon-arrow-select/icon-arrow-select';
 import MainScreen from '../../pages/main-screen/main-screen';
 
 type AppScreenProps = {
   placesCount: number;
 }
 
-export default function App({placesCount}: AppScreenProps): JSX.Element {
-  return <MainScreen placesCount = {placesCount} />;
+export default function App({ placesCount }: AppScreenProps): JSX.Element {
+  return (
+    <React.Fragment>
+      <IconArrowSelected />
+      <MainScreen placesCount={placesCount} />
+    </React.Fragment>
+  );
 }
