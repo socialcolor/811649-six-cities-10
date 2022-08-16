0431a2693a2a08b2104b3de0534eb0c893707f5e
socialcolor <64646814+socialcolor@users.noreply.github.com>
2022-08-16 04:50:47 +0300

    Изменил иконки маркеров

--- project/src/const.ts
+++ project/src/const.ts
@@ -1,3 +1,4 @@
+
 const ACTIVE_OFFER_ID = 0;
 
 const enum AppRoute {
@@ -25,9 +26,9 @@ const city = {
 
 const zoom = 12;
 
-const URL_MARKER_DEFAULT = 'https://assets.htmlacademy.ru/content/intensive/javascript-1/demo/interactive-map/pin.svg';
+const URL_MARKER_DEFAULT = '/img/pin.svg';
 
-const URL_MARKER_CURRENT = 'https://assets.htmlacademy.ru/content/intensive/javascript-1/demo/interactive-map/main-pin.svg';
+const URL_MARKER_CURRENT = '/img/pin-active.svg';
 
 
 export { AppRoute, AuthorizationStatus, ACTIVE_OFFER_ID, city, URL_MARKER_DEFAULT, URL_MARKER_CURRENT, zoom };
