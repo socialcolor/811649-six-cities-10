a7df6c2f9dde87c2335e44dffbad7139dc6a7e64
socialcolor <64646814+socialcolor@users.noreply.github.com>
2022-08-16 05:01:55 +0300

    Создал компонент сорт

--- project/src/components/offers-list/offers-list.tsx
+++ project/src/components/offers-list/offers-list.tsx
@@ -1,5 +1,6 @@
 import Offer from '../offer/offer';
 import { Offer as OfferType, Offers } from '../../types/offer';
+import Sort from '../sort/sort';
 
 type OfferListProps = {
   offers: Offers;
@@ -14,21 +15,7 @@ export default function OffersList({offers, currentCityName, onOfferHover, onOut
     <section className="cities__places places">
       <h2 className="visually-hidden">Places</h2>
       <b className="places__found">{offers.length} places to stay in {currentCityName}</b>
-      <form className="places__sorting" action="/#" method="get">
-        <span className="places__sorting-caption">Sort by</span>
-        <span className="places__sorting-type" tabIndex={0}>
-          Popular
-          <svg className="places__sorting-arrow" width="7" height="4">
-            <use xlinkHref="#icon-arrow-select"></use>
-          </svg>
-        </span>
-        <ul className="places__options places__options--custom places__options">
-          <li className="places__option places__option--active" tabIndex={0}>Popular</li>
-          <li className="places__option" tabIndex={0}>Price: low to high</li>
-          <li className="places__option" tabIndex={0}>Price: high to low</li>
-          <li className="places__option" tabIndex={0}>Top rated first</li>
-        </ul>
-      </form>
+      <Sort />
       <div className="cities__places-list places__list tabs__content">
         {offers.map((offer) => <Offer key={offer.id} offer={offer} onOfferHover={onOfferHover} onOutOfOffer={onOutOfOffer} />)}
       </div>
