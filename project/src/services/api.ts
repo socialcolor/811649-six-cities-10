f2c0e48f2976cff6a58a87c5f030f0252d386919
socialcolor <64646814+socialcolor@users.noreply.github.com>
2022-08-17 22:41:28 +0300

    timeout

--- project/src/services/api.ts
+++ project/src/services/api.ts
@@ -1,10 +1,12 @@
 import axios, { AxiosInstance } from 'axios';
 
 const BASE_URL = 'https://10.react.pages.academy/six-cities/';
+const REQUEST_TIMEOUT = 5000;
 
 export const createAPI = (): AxiosInstance => {
   const api = axios.create({
     baseURL: BASE_URL,
+    timeout: REQUEST_TIMEOUT,
   });
 
   return api;
