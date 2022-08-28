b7e197ce8c513680abb56646c0fdb1d02a0a07f5
socialcolor <64646814+socialcolor@users.noreply.github.com>
2022-08-28 21:30:04 +0300

    Fix. При отсутствии избранного отображается Nothing yet saved

--- project/src/pages/favorites-screen/favorites-screen.tsx
+++ project/src/pages/favorites-screen/favorites-screen.tsx
@@ -42,8 +42,15 @@ export default function FavoritesScreen({ authorizationStatus }: FavoritesScreen
       <main className="page__main page__main--favorites">
         <div className="page__favorites-container container">
           <section className="favorites">
-            <h1 className="favorites__title">Saved listing</h1>
-            {cities.map((city) => <FavoritesList key={city} offers={dict[city]} name={city} />)}
+            {offers && <h1 className="favorites__title">Saved listing</h1> && cities.map((city) => <FavoritesList key={city} offers={dict[city]} name={city} />)}
+            {!offers.length &&
+              <>
+                <h1 className="visually-hidden">Favorites (empty)</h1>
+                <div className="favorites__status-wrapper">
+                  <b className="favorites__status">Nothing yet saved.</b>
+                  <p className="favorites__status-description">Save properties to narrow down search or plan your future trips.</p>
+                </div>
+              </>}
           </section>
         </div>
       </main>
